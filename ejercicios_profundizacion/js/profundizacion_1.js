9dc9078d38b9d6110b659838156ef27ce2795776
Ignacio Serrano <johndoe@example.com>
2022-06-24 10:20:09 -0300

    Profu

--- ejercicios_profundizacion/js/profundizacion_1.js
+++ ejercicios_profundizacion/js/profundizacion_1.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * @fileoverview Ejercitación de Funciones
  * @author       Inove coding School <alumnos@inove.com.ar>
@@ -12,3 +13,25 @@
  *    Debe recibir 5 números como argumento y calcular el promedio.  Debe reportar por
  *    medio de alert(); y console.log(); el detalle de los datos recibidos.
 */
+let n1 = 0;
+let n2 = 0;
+let n3 = 0;
+let n4 = 0;
+let n5 = 0;
+
+function inputData() {
+    n1 = parseInt(prompt("Ingrese un número 1 para analizar"));
+    n2 = parseInt(prompt("Ingrese un número 2 para analizar"));
+    n3 = parseInt(prompt("Ingrese un número 3 para analizar"));
+    n4 = parseInt(prompt("Ingrese un número 4 para analizar"));
+    n5 = parseInt(prompt("Ingrese un número 5 para analizar"));
+}
+function promedio(a, b, c, d, e) {
+    if ((n1 !== NaN) && (n2 !== NaN) && (n3 !== NaN) && (n4 !== NaN) && (n5 !== NaN)){
+        alert(`El promedio de los numeros ${n1}, ${n2}, ${n3}, ${n4}, ${n5} es: ${(n1+n2+n3+n4+n5) / 5}`);
+        console.log((n1+n2+n3+n4+n5) / 5);
+    } else{
+        alert('Ingrese valores numericos.')
+    }
+    
+}
--- ejercicios_profundizacion/js/profundizacion_2.js
+++ ejercicios_profundizacion/js/profundizacion_2.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
  * @fileoverview Ejercitación de Funciones
  * @author       Inove coding School <alumnos@inove.com.ar>
@@ -13,3 +14,31 @@
  *    Según la cantidad especificada utilice condicionales para solicitar la cantidad de nombres
  *    correctos.
 */
+let cant = prompt('Ingrese cantidad de invitados de 1 a 3');
+let res;
+function generarInvitados(){
+    if (cant >0 && cant < 4){
+        
+        if (cant == 1){
+            let nom1 = prompt('Ingrese nombre del invitado 1');
+            let res = ("Los nombres de los invitados son: " + nom1);
+        }else if (cant == 2){
+            let nom1 = prompt('Ingrese nombre del invitado 1');
+            let nom2 = prompt('Ingrese nombre del invitado 2');
+            let res = ("Los nombres de los invitados son: " + nom1 + ", " + nom2);
+        }else{
+            let nom1 = prompt('Ingrese nombre del invitado 1');
+            let nom2 = prompt('Ingrese nombre del invitado 2');
+            let nom3 = prompt('Ingrese nombre del invitado 3');
+            let res = ("Los nombres de los invitados son: " + nom1 + ", " + nom2 + ", " + nom3);
+        }
+    }else{
+        alert('La cantidad debe estar entre 1 y 3.')
+    }
+    return(res)
+}
+
+let invitados = '';
+invitados = generarInvitados();
+alert(`Mis invitados: ${invitados}`);
+alert("FIN Ejercicio Profu 2.");
